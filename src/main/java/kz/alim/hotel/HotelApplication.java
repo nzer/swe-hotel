14c740eaa667a3e2c879d4c88fefb911f483b87c
Kuanysh Iliya <kuanysh.iliya@nu.edu.kz>
2020-11-03 20:25:04 +0600

    houston, trouble

--- src/main/java/kz/alim/hotel/HotelApplication.java
+++ src/main/java/kz/alim/hotel/HotelApplication.java
@@ -7,7 +7,9 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class HotelApplication {
 
 	public static void main(String[] args) {
+
 		SpringApplication.run(HotelApplication.class, args);
+		//comment
 	}
 
 }
