de4649154de6ab49d0d9d4bd4eb0e18005f9927c
alim <alim.naizabek@gmail.com>
2020-11-03 21:22:40 +0600

    fix request 3

--- src/main/java/kz/alim/hotel/data/RoomRepository.java
+++ src/main/java/kz/alim/hotel/data/RoomRepository.java
@@ -9,6 +9,7 @@ import java.time.LocalDateTime;
 import java.util.List;
 
 public interface RoomRepository extends CrudRepository<Room, Long> {
-    @Query("select r1, r2 from Room r1 join Reservation r2 on r2.Room = r1 where r2.End < ?2 and r2.Start > ?3 and r1.Hotel = ?1")
+    //@Query("select r1, r2 from Room r1 left join Reservation r2 on r2.Room = r1 and r2.End < ?2 and r2.Start > ?3 and r1.Hotel = ?1")
+    @Query("select r1 from Room r1 where not exists (select r2 from Reservation r2 where r2.Room = r1 and r2.End <= ?3 and r2.Start >= ?2 and r1.Hotel = ?1)")
     List<Room> findFreeRooms(Hotel hotel, LocalDateTime start, LocalDateTime finish);
 }
