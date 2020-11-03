e79ac8dbf3b691e8a4fc82019f0ef762d4687165
alim <alim.naizabek@gmail.com>
2020-11-03 23:51:19 +0600

    fix query

--- src/main/java/kz/alim/hotel/data/ReservationRepository.java
+++ src/main/java/kz/alim/hotel/data/ReservationRepository.java
@@ -8,6 +8,6 @@ import org.springframework.data.repository.CrudRepository;
 import java.util.List;
 
 public interface ReservationRepository extends CrudRepository<Reservation, Long> {
-    @Query("select Reservation from Reservation where Reservation.PayingGuest.Id = ?1")
+    @Query("select r from Reservation r where r.PayingGuest.Id = ?1")
     List<Reservation> findByPayingGuestId(Long guestId);
 }
